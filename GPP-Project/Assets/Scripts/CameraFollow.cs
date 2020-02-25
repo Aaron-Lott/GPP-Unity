8c78ab3c3fc95998f12559a22966248911910894
Aaron <aaronjameslott@gmail.com>
2020-02-25 12:25:34 +0000

    Camera work started

--- GPP-Project/Assets/Scripts/CameraFollow.cs
+++ GPP-Project/Assets/Scripts/CameraFollow.cs
@@ -9,8 +9,13 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset; //new Vector3(0, -2, 5);
     private Vector3 vectorRef;
 
+    private Vector3 maxOffset;
+    private Vector3 minOffset;
+
 
     public float smoothTime = 0.15f;
+
+    public float collisionSmoothTime = 0.5f;
 
     [HideInInspector]
     public bool rotateWithInput = true;
@@ -21,22 +26,57 @@ public class CameraFollow : MonoBehaviour
 
     void Start()
     {
+        maxOffset = offset * 2;
+        minOffset = offset;
     }
 
     void LateUpdate()
     {
-        if(rotateWithInput)
+        if (rotateWithInput)
         {
             turnAngleHorizontal = Quaternion.AngleAxis(Input.GetAxis("RightStickX") * rotationSpeed, Vector3.up);
 
             //Quaternion turnAngleVertical = Quaternion.AngleAxis(-Input.GetAxis("RightStickY") * rotationSpeed, target.right);
         }
 
+        if(Input.GetAxis("RightStickY") > 0.1f)
+        {
+           offset = Vector3.Slerp(offset, maxOffset, Input.GetAxis("RightStickY"));
+        }
+        else if(Input.GetAxis("RightStickY") < -0.1f)
+        {
+            offset = Vector3.Slerp(offset, minOffset, Mathf.Abs(Input.GetAxis("RightStickY")));
+        }
 
         offset = turnAngleHorizontal * offset;
 
+        //transform.position = Vector3.Slerp(transform.position, target.position - offset, smoothTime);
+        //transform.position = Vector3.Slerp(transform.position, target.position, collisionSmoothTime);
+
+
         transform.LookAt(target.position);
 
-        transform.position = Vector3.Slerp(transform.position, target.position - offset, smoothTime);
+        RayCastFunc();
+
+    }
+
+    bool RayCastFunc()
+    {
+        Vector3 direction = transform.position - target.position;
+        RaycastHit hit;
+
+        if(Physics.Raycast(target.position, direction, out hit, direction.magnitude))
+        {
+            Debug.DrawRay(target.position, direction, Color.yellow);
+            transform.position = target.position;
+            return true;
+        }
+        else
+        {
+            transform.position = Vector3.Slerp(transform.position, target.position - offset, smoothTime);
+        }
+
+        return false;
     }
 }
+
